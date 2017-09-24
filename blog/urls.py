fe6fddc8229803db7caf4c8cb73f56ac908d065a
ErinAppleton15 <Erinappleton@mac.com>
2017-09-24 11:49:02 +0200

    fix my brain

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+from django.shortcuts import render
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
