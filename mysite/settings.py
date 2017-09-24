dacf572fb9d63ea8bdf341a9f82e642230d8fcac
ErinAppleton15 <Erinappleton@mac.com>
2017-09-24 12:45:05 +0200

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')#kvflyd#)^*5$^e=fyx7kbq(ot4msrai$zv#%=59@g9$dq@d9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ErinAppleton15.pythonanywhere.com']
 
